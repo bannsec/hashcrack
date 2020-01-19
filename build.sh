db37f4b026dd2b2e0e462b9708ccb30a00eb16a0
bannsec <whootandahalf@gmail.com>
2020-01-19 13:16:08 -0500

    Unfucking rockyou again

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd . &>/dev/null
 
 cd $DIR
-git checkout hashcrack/static/wordlists/rockyou.txt.xz
+git checkout 22827be -- hashcrack/static/wordlists/rockyou.txt.xz
 rm hashcrack/static/wordlists/rockyou.txt
 
 python setup.py sdist
